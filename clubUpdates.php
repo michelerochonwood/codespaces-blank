e0e7373e28a182375dd1c2a5472bc8978aca6c8c
Michele Rochon-Wood <113383922+michelerochonwood@users.noreply.github.com>
2023-03-05 19:57:57 -0500

    Create clubUpdates.php

---
+++ clubUpdates.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content = "IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Update Club Lab 3 ROCHON-WOOD</title>
+</head>
+<body>
+<?php
+    include_once 'lab3.php';
+    $sql = "INSERT INTO clubs" (clubName, ground) VALUES
+   $clubName = $_POST['clubName'];
+   $ground = $_POST ['ground'];
+
+   ?>
+   </body>
+   </html>
